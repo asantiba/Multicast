cb3f2a0b93d7b90fcb059181a641798ff93b2056
Alniroza <alrodzam@gmail.com>
2018-10-18 20:26:15 -0300

    No se que wea

--- Multicast/src/Multicast2/MulticastPublisher.java
+++ Multicast/src/Multicast2/MulticastPublisher.java
@@ -1,5 +1,19 @@
 package Multicast2;
 
 public class MulticastPublisher {
-
+    private DatagramSocket socket;
+    private InetAddress group;
+    private byte[] buf;
+ 
+    public void multicast(
+      String multicastMessage) throws IOException {
+        socket = new DatagramSocket();
+        group = InetAddress.getByName("230.0.0.0");
+        buf = multicastMessage.getBytes();
+ 
+        DatagramPacket packet 
+          = new DatagramPacket(buf, buf.length, group, 4446);
+        socket.send(packet);
+        socket.close();
+    }
 }
