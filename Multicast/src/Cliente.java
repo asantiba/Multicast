7a985260d926e51c7f566e2a3b8a2851e26705c7
asantiba <alexis.santibanez.14@sansano.usm.cl>
2018-10-19 01:47:51 -0300

    Multicast OK

--- Multicast/src/Cliente.java
+++ Multicast/src/Cliente.java
@@ -8,21 +8,27 @@ public class Cliente {
 	JTextArea area = null;
 	JPanel container_area = null;
 	JScrollPane scroll = null;
-	// Variables socket uni
+
+	// Variables Multicast
+	private InetAddress group; // Grupo para unirse al multicast
+	protected MulticastSocket multicast_socket = null;
+	protected byte[] receiver_buf = new byte[256]; // Recibe los datos enviados por Multicast
+
+	// Variables cliente-servidor
 	Socket socket = null;
-	BufferedReader reader = null; //Permite leer mensajes
-	PrintWriter writer = null; //Permite enviar mensajes
-	
+	BufferedReader reader = null; // Permite leer mensajes
+	PrintWriter writer = null; // Permite enviar mensajes
+
 	public Cliente() {
 		Interface();
 	}
-	
+
 	public void Interface() {
 		window = new JFrame("Cliente");
-		area = new JTextArea(20,4);
+		area = new JTextArea(20, 4);
 		scroll = new JScrollPane(area);
 		container_area = new JPanel();
-		container_area.setLayout(new GridLayout(1,1));
+		container_area.setLayout(new GridLayout(1, 1));
 		container_area.add(scroll);
 		window.setLayout(new BorderLayout());
 		window.add(container_area, BorderLayout.NORTH);
@@ -34,26 +40,39 @@ public class Cliente {
 		Thread init = new Thread(new Runnable() {
 			public void run() {
 				try {
-					socket = new Socket("localhost", 9000);
-					read();
-				}catch(Exception ex) {
+					// socket = new Socket("localhost", 9000);
+					// read();
+					group = InetAddress.getByName("230.0.0.4");
+					multicast_socket = new MulticastSocket(10033);
+					multicast_socket.joinGroup(group);
+					while (true) {
+						// socket = server.accept();
+						DatagramPacket packet = new DatagramPacket(receiver_buf, receiver_buf.length);
+						multicast_socket.receive(packet);
+						String received = new String(packet.getData(), 0, packet.getLength());
+						area.append(received);
+						if ("end".equals(received)) {
+							break;
+						}
+					}
+				} catch (Exception ex) {
 					ex.printStackTrace();
 				}
 			}
 		});
 		init.start();
 	}
-	
+
 	public void read() {
 		Thread thread_reader = new Thread(new Runnable() {
 			public void run() {
 				try {
 					reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-					while(true) {
+					while (true) {
 						String msg_received = reader.readLine();
 						area.append(msg_received);
 					}
-				}catch(Exception ex) {
+				} catch (Exception ex) {
 					ex.printStackTrace();
 				}
 			}
--- Multicast/src/Servidor.java
+++ Multicast/src/Servidor.java
@@ -14,41 +14,58 @@ public class Servidor {
 	JTextArea area = null;
 	JScrollPane scroll = null;
 	JPanel container_area = null;
-	// Variables servidor
+
+	// Variables Multicast
+	private DatagramSocket datagram_socket; // Datagrama para enviar datos
+	private InetAddress group; // Grupo para unirse al multicast
+	private byte[] buf; // Buffer para enviar datos
+	protected MulticastSocket multicast_socket = null; //Socket Multicast
+	protected byte[] receiver_buf = new byte[256]; //Recibe los datos enviados por Multicast
+
+	// Variables cliente-servidor
 	ServerSocket server = null;
 	Socket socket = null;
-	BufferedReader reader = null; //Permite leer mensajes
-	PrintWriter writer = null; //Permite enviar mensajes
-	
+	BufferedReader reader = null; // Permite leer mensajes
+	PrintWriter writer = null; // Permite enviar mensajes
+
 	// constructor: Aquí se inicializa todo
 	public Servidor() {
 		Interface();
 	}
-	
+
 	public void Interface() {
-		window = new JFrame("Servidor"); //Ventana con nombre Servidor
-		area = new JTextArea(20,4);	//Area para insertar texto
-		scroll = new JScrollPane(area); //Barra para desplazar hacia abajo
+		window = new JFrame("Servidor"); // Ventana con nombre Servidor
+		area = new JTextArea(20, 40); // Area para insertar texto
+		scroll = new JScrollPane(area); // Barra para desplazar hacia abajo
 		container_area = new JPanel();
-		container_area.setLayout(new GridLayout(1,1));
+		container_area.setLayout(new GridLayout(1, 1));
 		container_area.add(scroll);
 		window.setLayout(new BorderLayout());
 		window.add(container_area, BorderLayout.NORTH);
 		window.setSize(500, 350);
 		window.setVisible(true);
-		window.setResizable(false); //Bloquea el cambio de tamaño 
+		window.setResizable(false); // Bloquea el cambio de tamaño
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		// Thread principal
 		Thread init = new Thread(new Runnable() {
 			public void run() {
 				try {
-					server = new ServerSocket(9000);
-					while(true) {
-						socket = server.accept();
-						read();
-						timer();
+					// server = new ServerSocket(9000);
+					group = InetAddress.getByName("230.0.0.4");
+					multicast_socket = new MulticastSocket(10033);
+					multicast_socket.joinGroup(group);
+					timer();
+					// Al parecer no podemos escuchar los mensajes que llegan al grupo, ME PEGAAAAAAAA EL PC, asi que la primera conexion debe ser por socket normal
+					/*
+					while (true) {
+						// Socket cliente-servidor
+						// socket = server.accept();
+
+						// Lee los mensajes enviados al grupo multicast
+						read_multicast();
 					}
-				}catch(Exception ex) {
+					*/
+				} catch (Exception ex) {
 					ex.printStackTrace();
 				}
 			}
@@ -56,76 +73,114 @@ public class Servidor {
 		init.start();
 	}
 	
-	public void read() {
+	public void read_multicast() {
 		Thread thread_reader = new Thread(new Runnable() {
 			public void run() {
 				try {
-					reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-					while(true) {
-						String msg_received = reader.readLine();
-						area.append("Cliente " + msg_received + " solicita historial\n");
+					DatagramPacket packet = new DatagramPacket(receiver_buf, receiver_buf.length);
+					multicast_socket.receive(packet);
+					String received = new String(packet.getData(), 0, packet.getLength());
+					//Si el mensaje solicita el historial enviarlo
+					if(received == "") {
+						area.append("Cliente " + received + " solicita historial\n");	
 					}
-				}catch(Exception ex) {
+				} catch (Exception ex) {
 					ex.printStackTrace();
 				}
 			}
 		});
 		thread_reader.start();
 	}
-	
+
+	public void read() {
+		Thread thread_reader = new Thread(new Runnable() {
+			public void run() {
+				try {
+					reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+					while (true) {
+						String msg_received = reader.readLine();
+						area.append("Cliente " + msg_received + " solicita historial\n");
+					}
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				}
+			}
+		});
+		thread_reader.start();
+	}
+
 	public void write(String number, String elemento) {
 		try {
 			writer = new PrintWriter(socket.getOutputStream(),true);
 			writer.println("Servidor: Variación de " + elemento + ": " + number + "\n");
+			// Envio de datos multicast
 			String line = "Variacion de " + elemento + ": " + number + "\n";
 			area.append(line);
-		}catch(Exception ex) {
+		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
 	}
 	
+	public void write_multicast(String number, String elemento) {
+		try {
+			// writer = new PrintWriter(socket.getOutputStream(),true);
+			// writer.println("Servidor: Variación de " + elemento + ": " + number + "\n");
+			// Envio de datos multicast
+			String line = "Variacion de " + elemento + ": " + number + "\n";
+			buf = line.getBytes();
+			datagram_socket = new DatagramSocket();
+			DatagramPacket packet = new DatagramPacket(buf, buf.length, group, 10033);
+			datagram_socket.send(packet);
+			area.append(line);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
 	public void timer() {
 		Thread thread_timer = new Thread(new Runnable() {
 			public void run() {
 				try {
 					// Datos NO2
 					ArrayList<String> NO2 = new ArrayList<String>();
-					String[] otherList = new String[] {"22.9", "16.5", "18.6", "11.2", "4.5", "20.1", "21.4", "14.7", "21.6", "12.6"};
+					String[] otherList = new String[] { "22.9", "16.5", "18.6", "11.2", "4.5", "20.1", "21.4", "14.7",
+							"21.6", "12.6" };
 					NO2.addAll(Arrays.asList(otherList));
 					// Datos CO
 					ArrayList<String> CO = new ArrayList<String>();
-					String[] otherList2 = new String[] {"0.37", "0.34", "0.59", "0.2", "0.16", "0.28", "0.31", "0.24", "0.55", "0.25"};
+					String[] otherList2 = new String[] { "0.37", "0.34", "0.59", "0.2", "0.16", "0.28", "0.31", "0.24",
+							"0.55", "0.25" };
 					CO.addAll(Arrays.asList(otherList2));
 					// Datos O3
 					ArrayList<String> O3 = new ArrayList<String>();
-					String[] otherList3 = new String[] {"0.023", "0.016", "0.017", "0.023", "0.04", "0.042", "0.039", "0.029", "0.038", "0.028"};
+					String[] otherList3 = new String[] { "0.023", "0.016", "0.017", "0.023", "0.04", "0.042", "0.039",
+							"0.029", "0.038", "0.028" };
 					O3.addAll(Arrays.asList(otherList3));
-					//Timer
+					// Timer
 					Timer timer = new Timer();
-					//Tarea a ejecutar
+					// Tarea a ejecutar
 					TimerTask task = new TimerTask() {
 						@Override
 						public void run() {
 							int number = (int) (Math.random() * 10);
-							write(NO2.get(number), "NO2");
+							write_multicast(NO2.get(number), "NO2");
 							number = (int) (Math.random() * 10);
-							write(CO.get(number), "CO");
+							write_multicast(CO.get(number), "CO");
 							number = (int) (Math.random() * 10);
-							write(O3.get(number), "O3");
+							write_multicast(O3.get(number), "O3");
 						}
 					};
-					timer.schedule(task, 0, 2000);
-				}catch(Exception ex) {
+					timer.schedule(task, 0, 10000);
+				} catch (Exception ex) {
 					ex.printStackTrace();
-				}				
+				}
 			}
 		});
 		thread_timer.start();
 	}
-	
+
 	public static void main(String[] args) {
 		new Servidor();
 	}
 
 }
-
